5664fdd85e439125c93baaf448893b3ec458c309
tracer-ics2017 <tracer@njuics.org>
2017-09-11 02:43:39 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  02:43:39 up 1 day,  8:20,  3 users,  load average: 0.10, 0.03, 0.01

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -73,7 +73,7 @@ static int cmd_x(char *args)
 {
   int length,address;
   sscanf(args,"%d 0x%x",&length,&address);
-  for(int i=0;i<length;i++) {
+  for(int i=0;i<length;++i) {
     if(!(i&0xf)) printf("\n0x%08x: ",address+i*16);
    // printf("0x%02x ",*(unsigned char *)hwa_to_va(address+i));
   }
