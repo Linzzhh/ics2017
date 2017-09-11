0801aacf29bfea687376f7bf892cadff8503a8b9
tracer-ics2017 <tracer@njuics.org>
2017-09-11 01:42:35 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  01:42:35 up 1 day,  7:19,  2 users,  load average: 0.00, 0.11, 0.47

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,10 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_s(char *args) {
+  cpu_exec(1);
+  return 0;
+}
 static struct {
   char *name;
   char *description;
@@ -46,8 +50,8 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
   /* TODO: Add more commands */
+  { "s", "Single Step", cmd_s},
 
 };
 
