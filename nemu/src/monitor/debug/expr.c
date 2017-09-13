74f31dbc5575eb5c3b7863a68ded1703773d9617
tracer-ics2017 <tracer@njuics.org>
2017-09-13 02:44:31 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  02:44:31 up 2 days,  5:28,  4 users,  load average: 0.01, 0.01, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -251,6 +251,7 @@ uint32_t expr(char *e, bool *success) {
   
   for(int i=0;i<nr_token;i++){
   	if(tokens[i].type=='*'&&(i==0||tokens[i-1].type!=NUM||tokens[i-1].type!=')'))
+		printf("%d   %c\n", tokens[i].type,tokens[i-1].type);
 	  tokens[i].type=DEREF;
   }
 
