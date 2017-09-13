c8bdbb577358926cdb11c5f07fda76f5a6147d19
tracer-ics2017 <tracer@njuics.org>
2017-09-13 02:05:58 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  02:05:58 up 2 days,  4:50,  4 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -8,7 +8,8 @@
 #include <stdlib.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,NUM
+  TK_NOTYPE = 256,NUM,HEX,REG
+	  , TK_EQ,TK_NEQ,LOGIC_AND,LOGIC_OR,NOT,DEREF
 
   /* TODO: Add more token types */
 
@@ -28,10 +29,19 @@ static struct rule {
   {"\\-", '-'},               // minus
   {"\\*", '*'},               // mul
   {"\\/", '/'},               // div
-  {"[0-9]+", NUM},          // decimal
+  {"[0-9]+", NUM},            // decimal
   {"\\(", '('},               // lbracket
   {"\\)", ')'},               // rbracket
   {"==", TK_EQ},              // equal
+  {"^0x[0-9a-fA-F]+", HEX},   // hex
+  {"^\\$[a-zA-Z]+", REG},     // reg_name
+  {"!=", TK_NEQ},             // not equal
+  {"&&", LOGIC_AND},          // logic and
+  {"\\|\\|", LOGIC_OR},       // logic or
+  {"!", '!'},                 // not
+ 
+
+
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -93,13 +103,24 @@ static bool make_token(char *e) {
 		case '/':
 		case '(':
 		case ')':
-		tokens[nr_token++].type=rules[i].token_type;break;
+		case '!':
+		case TK_EQ:
+		case TK_NEQ:
+		case LOGIC_AND:
+		case LOGIC_OR:
+	          tokens[nr_token++].type=rules[i].token_type;break;
 		case NUM:
-		tokens[nr_token].type=rules[i].token_type;
-		for(int j=0;j<substr_len;j++)
+		case HEX:
+		  tokens[nr_token].type=rules[i].token_type;
+		  for(int j=0;j<substr_len;j++)
 			tokens[nr_token].str[j]=substr_start[j];
-		nr_token++;
+	          nr_token++;
 		break;
+		case REG:
+		  tokens[nr_token].type=rules[i].token_type;
+		  for(int j=1;j<substr_len;j++)
+			tokens[nr_token].str[j-1]=substr_start[j];
+                  nr_token++;
 		default: TODO();
         }
 
@@ -122,8 +143,11 @@ static bool make_token(char *e) {
 
 static int get_level(int op)
 {
-	if(op=='+'||op=='-') return 1;
-	if(op=='*'||op=='/') return 2;
+	if(op=='!'||op==DEREF) return 1;
+	if(op=='+'||op=='-') return 2;
+	if(op=='*'||op=='/') return 3;
+	if(op==TK_EQ||op==TK_NEQ) return 4;
+	if(op==LOGIC_AND||op==LOGIC_OR) return 5;
 	return 10000;
 }
 static int cmp_op(int op1,int op2)
@@ -140,13 +164,12 @@ static int get_dominant_optype(int p,int q){//get the index...
 	int index=p;
 	for(int i=p;i<=q;i++){
 		int tp=tokens[i].type;
-		if(tp<TK_NOTYPE){
+		if(tp<TK_NOTYPE||tp>REG){ //the type is a operation
 		if(tp=='(') {inbracket++;continue;}
 		if(tp==')') {inbracket--;continue;}
 		if(inbracket>0) continue; // in the bracket..
                 if(cmp_op(op,tp)>=0) {op=tp;index=i;}
-		}
-		else continue; //not operation..
+	}
 	}
 	return index;
 }
@@ -168,30 +191,44 @@ static bool check_parentheses(int p, int q)
 	return true;
 }
 static uint32_t eval(int p,int q)
-{       printf("%d       %d\n",p,q);
-	if(p>q){
+{	if(p>q){
 	  printf("bad expression!!"); return -1;
 	}
 	else if(p==q){
-		printf("%s\n",tokens[p].str);
-		return atoi(tokens[p].str);
+                if(tokens[p].type==NUM) //the value is the decimal
+	          return atoi(tokens[p].str);
+		else if(tokens[p].type==HEX) //the value is the hex
+		  return strtol(tokens[p].str,NULL,16);
+		else if(tokens[p].type==REG){// the value is register
+		for(int i=R_EAX;i<=R_EDI;i++)
+			if(strcmp(regsl[i],tokens[p].str)==0){
+			return cpu.gpr[i]._32;
+			}
+		}
+		  return -1;
 	  // return the value of number
 	}
 	else if(check_parentheses(p,q)==true){
 	//the expression is surrouded by a match
- 		printf("match!!!!");
       		return eval(p+1,q-1);
 	}else{
 	  int op=get_dominant_optype(p,q);
-	  printf("index is %d",op);
-	  int val1=eval(p,op-1);
-	  int val2=eval(op+1,q);
-	  char optype=tokens[op].type;
+	  int optype=tokens[op].type;
+	  int val1,val2;
+	  if(optype!='!'&&optype!=DEREF)
+	    val1=eval(p,op-1);
+	  val2=eval(op+1,q);
 	  switch(optype){
 		case '+':return val1+val2;
 		case '-':return val1-val2;
 		case '*':return val1*val2;
 		case '/':return val1/val2;
+		case TK_NEQ:return val1!=val2;
+		case LOGIC_AND:return val1&&val2;
+		case LOGIC_OR:return val1||val2;
+		case TK_EQ:return val1==val2;
+		case DEREF: vaddr_read(val2,4);
+		case '!':return !val2;
 		default :assert(0);
 	}
 	}
@@ -208,7 +245,13 @@ uint32_t expr(char *e, bool *success) {
  // else printf("error!!!!!!!");
  //int index =  get_dominant_optype(0,nr_token);
  //printf("%c %d",tokens[index].type,index);
-  int value=eval(0,nr_token-1);
-  printf("%d",value);
+  
+  for(int i=0;i<nr_token;i++){
+  	if(tokens[i].type=='*'&&(i==0||tokens[i-1].type!=NUM))
+	  tokens[i].type=DEREF;
+  }
+
+  uint32_t value=eval(0,nr_token-1);
+  printf("%d  0x%08x \n",value,value);
   return 0;
 }
