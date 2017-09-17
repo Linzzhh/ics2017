7d93769437bd4d66c4e9938a5f225dac95b06d88
tracer-ics2017 <tracer@njuics.org>
2017-09-17 08:37:25 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  08:37:25 up 1 day, 17:17, 10 users,  load average: 0.00, 0.04, 0.00

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -54,7 +54,7 @@ static inline make_DopHelper(SI) {
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
- // snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
+  snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
 #endif
 }
 
@@ -273,7 +273,7 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  decoding.jmp_eip = id_dest->simm + *eip;
+ // decoding.jmp_eip = id_dest->simm + *eip;
 }
 
 make_DHelper(push_SI) {
