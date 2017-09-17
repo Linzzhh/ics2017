7e3b363bd9804ad90dcec3cd3e57dcad125140d7
tracer-ics2017 <tracer@njuics.org>
2017-09-17 08:47:41 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  08:47:41 up 1 day, 17:27,  4 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,7 +25,7 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  printf("hahahaha");
+  printf("hahahaha ");
   rtl_push(&decoding.seq_eip);
   decoding.jmp_eip=decoding.seq_eip+id_dest->val;
   printf("0x%08x",decoding.jmp_eip);
