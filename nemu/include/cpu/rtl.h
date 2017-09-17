c9cdae498e9fc03594b8397b4b8881c184725386
tracer-ics2017 <tracer@njuics.org>
2017-09-17 06:14:50 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  06:14:50 up 1 day, 14:57,  9 users,  load average: 0.00, 0.00, 0.00

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -143,7 +143,9 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
  // TODO();
+  printf("0x%08x",cpu.esp);
   cpu.esp=cpu.esp-4;
+  printf("0x%08x",cpu.esp);
   vaddr_write((cpu.esp), 4, *src1);
 
 }
