91349ae189015bef87390b17d43aa7b60bc85bda
tracer-ics2017 <tracer@njuics.org>
2017-09-17 11:56:23 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  11:56:23 up 1 day, 19:06,  4 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -80,6 +80,7 @@ static inline make_DopHelper(a) {
  */
 static inline make_DopHelper(r) {
   op->type = OP_TYPE_REG;
+  op->reg =decoding.opcode & 0x7; 
   if (load_val) {
     rtl_lr(&op->val, op->reg, op->width);
   }
