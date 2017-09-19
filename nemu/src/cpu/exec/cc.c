11885edace30cbd04039d13713fa28f549d9480c
tracer-ics2017 <tracer@njuics.org>
2017-09-19 06:30:14 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  06:30:14 up 2 days, 13:41,  8 users,  load average: 0.06, 0.03, 0.01

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -4,7 +4,6 @@
 
 void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   bool invert = subcode & 0x1;
-  printf("%d  0x%08x  %d\n",subcode,subcode,invert);
   enum {
     CC_O, CC_NO, CC_B,  CC_NB,
     CC_E, CC_NE, CC_BE, CC_NBE,
@@ -15,14 +14,15 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+	  case CC_O:  rtl_get_OF(&t0); *dest=t0; break;
+	  case CC_B:  rtl_get_CF(&t0); *dest=t0; break;
+	  case CC_E:  rtl_get_ZF(&t0); *dest=t0; break;
+          case CC_BE: rtl_get_ZF(&t0); rtl_get_CF(&t1); *dest=t0|t1; break;
+	  case CC_S:  rtl_get_SF(&t0); *dest=t0; break;
+          case CC_L:  rtl_get_SF(&t0); rtl_get_OF(&t1); *dest=(t0!=t1); break;
+	  case CC_LE: rtl_get_SF(&t0); rtl_get_OF(&t1);rtl_get_ZF(&t2); 
+		      *dest=((!t2)&(t0!=t1)); break;
+   //   TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
