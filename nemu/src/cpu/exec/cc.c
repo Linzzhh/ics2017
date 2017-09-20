3f43f6ccc9e31bc726ac53e090818b24a7a14fdb
tracer-ics2017 <tracer@njuics.org>
2017-09-20 06:14:53 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  06:14:53 up 3 days, 57 min,  8 users,  load average: 0.07, 0.02, 0.00

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -18,7 +18,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 	  case CC_B:  rtl_get_CF(&t0); *dest=t0; break;
 	  case CC_E:  rtl_get_ZF(&t0); 
 		      *dest=t0;  break;
-	  case CC_BE: rtl_get_ZF(&t0); rtl_get_CF(&t1); *dest=t0|t1; 
+	  case CC_BE: rtl_get_ZF(&t0); rtl_get_CF(&t1); *dest=t0|t1;
+		       printf("%d   %d  %d~~~~~\n",t0,t1,*dest); 
 		       break;
 	  case CC_S:  rtl_get_SF(&t0); *dest=t0; 
 		      break;
