57bc156a8656cfd8c4d4a775ecf7c0d7ebc6038b
tracer-ics2017 <tracer@njuics.org>
2017-09-20 01:26:06 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  01:26:06 up 2 days, 21:18,  8 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,7 +2,6 @@
 
 make_EHelper(add) {
  // TODO();
- // printf("!!!!!");
   rtl_add(&t2, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t2);
 
@@ -52,10 +51,15 @@ make_EHelper(cmp) {
   
   rtl_update_ZFSF(&t2, id_dest->width);
   
-  t0=0;
-  rtl_set_CF(&t0);
-  rtl_set_OF(&t0);
   
+  rtl_sltu(&t0, &id_dest->val, &t2);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
   //TODO();
   print_asm_template2(cmp);
 }
