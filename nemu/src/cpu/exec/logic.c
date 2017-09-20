db42b93e44621f9f365310ad9a07e52bcf58e69d
tracer-ics2017 <tracer@njuics.org>
2017-09-20 05:00:18 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  05:00:18 up 2 days, 23:44,  8 users,  load average: 0.04, 0.06, 0.02

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -115,6 +115,7 @@ make_EHelper(shr) {
 }
 
 make_EHelper(setcc) {
+  printf("decoding   0x%08x \n ",decoding.opcode);
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
   operand_write(id_dest, &t2);
