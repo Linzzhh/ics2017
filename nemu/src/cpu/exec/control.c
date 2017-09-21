b8e694cf5a5b1a63874c25742130c6a65d483971
tracer-ics2017 <tracer@njuics.org>
2017-09-21 09:05:12 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  09:05:12 up 3 days, 15:13,  8 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -44,7 +44,7 @@ make_EHelper(call_rm) {
  // TODO();
   
   rtl_push(&decoding.seq_eip);
-  decoding.jmp_eip=decoding.seq_eip+id_dest->val;
+  decoding.jmp_eip=id_dest->val;
   decoding.is_jmp = 1 ;
   print_asm("call *%s", id_dest->str);
 }
