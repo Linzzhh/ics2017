4c6cf47559985fddd49c3665c84b6f18a824cc59
tracer-ics2017 <tracer@njuics.org>
2017-09-21 05:26:54 +0000

    >  compile 161220000 ics Linux 1aa2d37a9ee4 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  05:26:54 up 3 days, 12:02,  8 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -103,7 +103,7 @@ make_EHelper(dec) {
   rtl_set_CF(&t0);
   
   t3=1;
-  rtl_xor(&t0, &id_dest->val, &t3);
+  //rtl_xor(&t0, &id_dest->val, &t3);
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_xor(&t1, &id_dest->val, &t2);
   rtl_and(&t0, &t0, &t1);
