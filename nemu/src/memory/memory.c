1db2a0da6d429ed2ce995156a391331034a47785
tracer-ics2017 <tracer@njuics.org>
2017-11-14 14:12:56 +0000

    >  compile 161220000 ics Linux 2ed932ac0443 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  14:12:56 up 41 min,  4 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -27,14 +27,14 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   memcpy(guest_to_host(addr), &data, len);
 }
 
-/*
+
 paddr_t page_translate(vaddr_t addr, bool is_write) {
   if (cpu.PG == 1) {
 	paddr_t page_dir_base = cpu.cr3;
 	paddr_t page_dir_off = (addr >> 22) << 2;
 	paddr_t pde_addr = page_dir_base + page_dir_off;
 	uint32_t pde = paddr_read(pde_addr, 4);//(pde)page_tab_base with 3bits U/S R/W P
-	if ((pde & 0x1) == 0) {//if page_tab present
+	if ((pde & 0x1) == 0) {//if page_tab not present
 	  Log("pde_base: 0x%-8x", page_dir_base);
 	  Log("pde: 0x%-8x", pde);
 	  Assert(0, "vaddr: 0x%-8x. The present bit of pde is wrong!!!", addr);
@@ -43,7 +43,7 @@ paddr_t page_translate(vaddr_t addr, bool is_write) {
 	paddr_t page_tab_off  = (addr & 0x003ff000) >> 10; //equal to >>12 <<2
 	paddr_t pte_addr = page_tab_base + page_tab_off; 
 	uint32_t pte = paddr_read(pte_addr, 4);//(pte)page_base with 3bits U/S R/W P
-	if ((pte & 0x1) == 0) {//if page present
+	if ((pte & 0x1) == 0) {//if page not present
 	  Log("pde: 0x%-8x", pde);
 	  Log("pte: 0x%-8x", pte);
 	  Assert(0, "The present bit of pte is wrong!!!");
@@ -56,38 +56,6 @@ paddr_t page_translate(vaddr_t addr, bool is_write) {
 	return page_base + (addr & 0x00000fff);
   } 
 	return addr;
-}
-*/
-
-paddr_t page_translate(vaddr_t addr, bool is_write) {
-  if (cpu.PG == 1) {
-	paddr_t pde_base = cpu.cr3;
-	paddr_t pde_addr = pde_base + ((addr >> 22) << 2);
-	uint32_t pde = paddr_read(pde_addr, 4);
-	if ((pde & 0x1) == 0) {
-	  Log("pde_base: 0x%-8x", pde_base);
-	  Log("pde: 0x%-8x", pde);
-	  Assert(0, "vaddr: 0x%-8x. The present bit of pde is wrong!!!", addr);
-	}
-	paddr_t pte_base = pde & 0xfffff000;
-	paddr_t pte_addr = pte_base + ((addr & 0x003ff000) >> 10);
-	uint32_t pte = paddr_read(pte_addr, 4);
-	if ((pte & 0x1) == 0) {
-	  Log("addr: 0x%-8x", addr);
-
-	  Log("pde: 0x%-8x", pde);
-	  Log("pte: 0x%-8x", pte);
-	  Assert(0, "The present bit of pte is wrong!!!");
-	}
-	paddr_t page_base = pte & 0xfffff000;
-	pte |= 0x20;
-	if (is_write)
-		pte |= 0x40;
-	paddr_write(pte_addr, 4, pte);
-	return page_base + (addr & 0x00000fff);
-  } else {
-	return addr;
-  }
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
@@ -111,7 +79,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
      return paddr_read(paddr, len);
   }
 }
-
+/*
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   //if addr+len goto next page and not the begin of page
   if ((((addr ^ (addr + len)) & 0x003ff000) != 0) && (((addr + len) & 0x00000fff) != 0)) { 
@@ -130,4 +98,28 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data) {
      paddr_t paddr = page_translate(addr, true);
      paddr_write(paddr, len, data);
   }
-}
+}*/
+
+
+void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+
+if ((((addr ^ (addr + len)) & 0x003ff000) != 0) && (((addr + len) & 0x00000fff) != 0)) {
+	// Here,the method is not so good, there should be modified!
+	int i;
+	for (i = 1; i < 4; i++)
+		if (((addr + i) & 0x00000fff) == 0)
+			break;
+	if (i == 4) {
+		Log("addr: 0x%-8x len: %d", addr, len);
+		Assert(0, "Data cross the page boudary!!!");
+	}
+	paddr_t paddr = page_translate(addr, true);
+	paddr_write(paddr, i, (data & (~0u >> ((4 - i) << 3))));
+	paddr = page_translate(addr+i, true);
+	paddr_write(paddr, len - i, (data >> (i << 3)));
+  } else {
+    paddr_t paddr = page_translate(addr, true);
+    paddr_write(paddr, len, data);
+  
+
+}}
